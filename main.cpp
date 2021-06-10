70aed8b4cd6afe9227981678347818fc76b464cd
“WayneTian-42” <arthassum@gmail.com>
2021-06-10 10:14:34 +0800

    主函数非法输入

--- main.cpp
+++ main.cpp
@@ -9,55 +9,28 @@ int main()
     std::cout << "请输入建立客户端还是服务端\n";
     int choice;
     std::cin >> choice;
+    while (std::cin.fail() || std::cin.get() != '\n')
+    {
+        std::cin.clear();
+        std::cin.ignore(LLONG_MAX, '\n');
+        std::cout << "输入不合法，请输入数字\n";
+        std::cin >> choice;
+        continue;
+    }
     if (choice == 0)
     {
-        /* Server server;
-        if (!server.serverInit())
-            return -1; */
         Platform sys;
-        // sys.initSock();
         sys.show();
         sys.freeUser();
         sys.freeGoods();
     }
     else if (choice == 1)
     {
-        /* Client client;
-        if (!client.clientInit())
-            return -1; */
         getchar();
         PlatformShow sysc;
         sysc.show();
     }
-
-    /* std::string name;
-    std::cout << "Please enter your name:\n";
-    std::cin >> name;
-    if (!name.size())
-        std::cout << "No input!";
     else
-    {
-        Account test{name};
-        if (!test.registerAcc())
-            std::cout << "The name has been used!\n";
-        test.changePwd();
-    } */
-    /* std::string iname;
-    int type;
-    std::cout << "Please enter item name:\n";
-    std::cin >> iname;
-    std::cout << "type:  ";
-    std::cin >> type;
-    if (type == 1)
-    {
-        Foods gtest1(iname);
-        gtest1.addItems();
-        std::cout << gtest1.getPrice();
-    }
-    else if (type == 2)
-        Clothes gtest2(iname);
-    else if (type == 3)
-        Books gtest3(iname); */
-    // system("pause");
+        std::cout << "选择错误，已退出\n";
     return 0;
 }
